87434a11b2030ee28e1aa74bf9c2592c016b62dd
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-03-16 12:55:17 -0700

    Added deletion cases for tag movement

--- codetagging/src/extension.ts
+++ codetagging/src/extension.ts
@@ -103,35 +103,64 @@ class Listener {
         vscode.workspace.onDidChangeTextDocument(this._onEvent, this, context.subscriptions);
     }
     _onEvent(e: vscode.TextDocumentChangeEvent) {
-        let horizontalMovement:number = 0;
-        let startLine:number = 0;
+        let changeStart:number = 0;
+        let changeEnd:number = 0;
+        let changeAddition:boolean = true;
         for (let msg of e.contentChanges) {
             if (msg) {
-                console.log(msg);
                 if (msg.text === "") {
-                    horizontalMovement = msg.range.start.line - msg.range.end.line;
+                    changeAddition = false;
+                    changeStart = msg.range.start.line;
+                    changeEnd = msg.range.end.line;
                 }
-                for (let i = 0; i < msg.text.length; i++) {
-                    console.log(msg.text.charCodeAt(i), msg.range.start.line);
-                    if (msg.text.charAt(i) === "\n") {
-                        horizontalMovement += 1;
+                if (changeAddition) {
+                    changeStart = msg.range.start.line;
+                    changeEnd = msg.range.end.line;
+                    for (let i = 0; i < msg.text.length; i++) {
+                        console.log(msg.text.charCodeAt(i), msg.range.start.line);
+                        if (msg.text.charAt(i) === "\n") {
+                            changeAddition = true;
+                            changeEnd += 1;
+                        }
                     }
                 }
-                startLine = msg.range.start.line;
-                console.log("horizontal: " + horizontalMovement + " from " + startLine);
             }
         }
+        let horizontalMovement:number = changeEnd - changeStart;
         for (let tag of tags) {
-            let oldStart:number = tag.start.line;
-            let oldEnd:number = tag.end.line;
-            if (tag.start.line < startLine && tag.end.line >= startLine) {
-                oldEnd += horizontalMovement;
-            } else if (tag.start.line >= startLine) {
-                oldStart += horizontalMovement;
-                oldEnd += horizontalMovement;
+            if (changeAddition && horizontalMovement !== 0) { // Addition of text
+                let newStart:number = tag.start.line;
+                let newEnd:number = tag.end.line;
+                if (tag.start.line < changeStart && tag.end.line >= changeStart) { // Addition is in between
+                    newEnd += horizontalMovement;
+                } else if (tag.start.line >= changeStart) { // Addition is before
+                    newStart += horizontalMovement;
+                    newEnd += horizontalMovement;
+                }
+                tag.start = new vscode.Position(newStart, 0);
+                tag.end = new vscode.Position(newEnd, 0);
+            } else if (!changeAddition) { // Deletion of text
+                let horizontalMovement:number = changeEnd - changeStart;
+                let newStart:number = tag.start.line;
+                let newEnd:number = tag.end.line;
+                if (changeEnd < tag.start.line) { // Deletion is above
+                    newStart += horizontalMovement;
+                    newEnd += horizontalMovement;
+                } else if (changeStart < tag.start.line && changeEnd >= tag.start.line && changeEnd < tag.end.line) { // around start
+                    newStart = changeEnd;
+                    newEnd += horizontalMovement;
+                } else if (changeStart >= tag.start.line && changeEnd < tag.end.line) { // within
+                    newEnd += horizontalMovement;
+                } else if (changeStart <= tag.start.line && changeEnd >= tag.end.line) { // around
+                    // TODO: Delete the tag
+                    newStart = 0;
+                    newEnd = 0;
+                } else if (changeStart > tag.start.line && changeEnd >= tag.end.line) { // around end
+                    newEnd = changeStart;
+                }
+                tag.start = new vscode.Position(newStart, 0);
+                tag.end = new vscode.Position(newEnd, 0);
             }
-            tag.start = new vscode.Position(oldStart, 0);
-            tag.end = new vscode.Position(oldEnd, 0);
         }
         redraw();
     }
