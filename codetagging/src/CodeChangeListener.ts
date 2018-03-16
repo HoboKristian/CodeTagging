a854ec32023aa558353fd6ff2efb469580e8356b
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-03-16 12:59:02 -0700

    Moved listener into its own class

---
+++ codetagging/src/CodeChangeListener.ts
@@ -0,0 +1,71 @@
+import * as vscode from 'vscode';
+
+export default class CodeChangeListener {
+    tagsMovedCallback: Function;
+    constructor(context: vscode.ExtensionContext, callback: Function) {
+        vscode.workspace.onDidChangeTextDocument(this._onEvent, this, context.subscriptions);
+        this.tagsMovedCallback = callback;
+    }
+    _onEvent(e: vscode.TextDocumentChangeEvent) {
+        let changeStart:number = 0;
+        let changeEnd:number = 0;
+        let changeAddition:boolean = true;
+        for (let msg of e.contentChanges) {
+            if (msg) {
+                if (msg.text === "") {
+                    changeAddition = false;
+                    changeStart = msg.range.start.line;
+                    changeEnd = msg.range.end.line;
+                }
+                if (changeAddition) {
+                    changeStart = msg.range.start.line;
+                    changeEnd = msg.range.end.line;
+                    for (let i = 0; i < msg.text.length; i++) {
+                        console.log(msg.text.charCodeAt(i), msg.range.start.line);
+                        if (msg.text.charAt(i) === "\n") {
+                            changeAddition = true;
+                            changeEnd += 1;
+                        }
+                    }
+                }
+            }
+        }
+        let horizontalMovement:number = changeEnd - changeStart;
+        for (let tag of tags) {
+            if (changeAddition && horizontalMovement !== 0) { // Addition of text
+                let newStart:number = tag.start.line;
+                let newEnd:number = tag.end.line;
+                if (tag.start.line < changeStart && tag.end.line >= changeStart) { // Addition is in between
+                    newEnd += horizontalMovement;
+                } else if (tag.start.line >= changeStart) { // Addition is before
+                    newStart += horizontalMovement;
+                    newEnd += horizontalMovement;
+                }
+                tag.start = new vscode.Position(newStart, 0);
+                tag.end = new vscode.Position(newEnd, 0);
+            } else if (!changeAddition) { // Deletion of text
+                let horizontalMovement:number = changeEnd - changeStart;
+                let newStart:number = tag.start.line;
+                let newEnd:number = tag.end.line;
+                if (changeEnd < tag.start.line) { // Deletion is above
+                    newStart += horizontalMovement;
+                    newEnd += horizontalMovement;
+                } else if (changeStart < tag.start.line && changeEnd >= tag.start.line && changeEnd < tag.end.line) { // around start
+                    newStart = changeEnd;
+                    newEnd += horizontalMovement;
+                } else if (changeStart >= tag.start.line && changeEnd < tag.end.line) { // within
+                    newEnd += horizontalMovement;
+                } else if (changeStart <= tag.start.line && changeEnd >= tag.end.line) { // around
+                    // TODO: Delete the tag
+                    newStart = 0;
+                    newEnd = 0;
+                } else if (changeStart > tag.start.line && changeEnd >= tag.end.line) { // around end
+                    newEnd = changeStart;
+                }
+                tag.start = new vscode.Position(newStart, 0);
+                tag.end = new vscode.Position(newEnd, 0);
+            }
+        }
+        this.tagsMovedCallback();
+    }
+}
