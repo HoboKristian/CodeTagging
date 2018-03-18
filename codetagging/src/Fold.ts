50e4ec5f6fba6ef4a4826b2fb78abb061280cde8
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-03-18 16:03:20 -0700

    Changing the folding code to functional programming.

--- codetagging/src/Fold.ts
+++ codetagging/src/Fold.ts
@@ -1,69 +1,55 @@
 'use strict';
 import * as vscode from 'vscode';
 import TagInfo from './TagInfo';
+import Tag from './Tag';
 import Singleton from './Singleton';
 
 let foldedMethods:string[] = [];
 
 namespace Fold {
     async function getMethodsInCurrentFiles() {
-        let methods:vscode.SymbolInformation[] = [];
         let textEditor = vscode.window.activeTextEditor;
         if (textEditor) {
-            await vscode.commands.executeCommand<vscode.SymbolInformation[]>('vscode.executeDocumentSymbolProvider', textEditor.document.uri).then((symbols: vscode.SymbolInformation[] | undefined) => {
-                if (symbols) {
-                    for (let si of symbols) {
-                        if (si.kind === vscode.SymbolKind.Method) {
-                            methods.push(si);
-                        }
-                    }
-                }
-            });
-        }
-        return methods;
-    }
-
-    function methodContainsHighlightedTag(method:vscode.SymbolInformation, hightlightedTagInfo:TagInfo):boolean {
-        let methodStart = method.location.range.start.line;
-        let methodEnd = method.location.range.end.line;
-        for (let tag of Singleton.getTags()) {
-            if (tag.tagInfo === hightlightedTagInfo) {
-                let tagStart = tag.start;
-                let tagEnd = tag.end;
-                if ((tagStart >= methodStart && tagStart <= methodEnd) ||
-                (tagEnd >= methodStart && tagEnd <= methodEnd)) {
-                    return true;
-                }
+            let symbols = await vscode.commands.executeCommand<vscode.SymbolInformation[]>('vscode.executeDocumentSymbolProvider', textEditor.document.uri);
+            if (symbols) {
+                return symbols.filter(element => element.kind === vscode.SymbolKind.Method);
             }
         }
-        return false;
+        return [];
+    }
+
+    function methodContainsHighlightedTags(method:vscode.SymbolInformation, highlightedTags:Tag[]):boolean {
+        let methodStart = method.location.range.start.line;
+        let methodEnd = method.location.range.end.line;
+        
+        return highlightedTags.some(tag => 
+            ((tag.start >= methodStart && tag.start <= methodEnd) ||
+            (tag.end >= methodStart && tag.end <= methodEnd)));
     }
 
     export async function unfoldFoldedMethods() {
         let textEditor = vscode.window.activeTextEditor;
         if (textEditor) {
-            vscode.commands.executeCommand<vscode.SymbolInformation[]>('vscode.executeDocumentSymbolProvider', textEditor.document.uri).then((symbols: vscode.SymbolInformation[] | undefined) => {
-                if (symbols) {
-                    let lineNumbers:number[] = [];
-                    for (let si of symbols) {
-                        if (si.kind === vscode.SymbolKind.Method && foldedMethods.includes(si.name)) {
-                            lineNumbers.push(si.location.range.start.line);
-                        }
-                    }
-                    vscode.commands.executeCommand('editor.unfold', {levels: 1, direction: 'up', selectionLines: lineNumbers});
-                    foldedMethods = [];
-                }
-            });
-        }
-    }
-    export async function highlightTag(tag:TagInfo) {
-        let linesToCollapse: number[] = [];
-        for (let method of await getMethodsInCurrentFiles()) {
-            if (!methodContainsHighlightedTag(method, tag)) {
-                foldedMethods.push(method.name);
-                linesToCollapse.push(method.location.range.start.line);
+            let symbols = await vscode.commands.executeCommand<vscode.SymbolInformation[]>('vscode.executeDocumentSymbolProvider', textEditor.document.uri);
+            if (symbols) {
+                let lineNumbers:number[] = symbols
+                .filter(si => (si.kind === vscode.SymbolKind.Method))
+                .filter(si => (foldedMethods.includes(si.name)))
+                .map(si => si.location.range.start.line);
+                vscode.commands.executeCommand('editor.unfold', {levels: 1, direction: 'up', selectionLines: lineNumbers});
+                foldedMethods = [];
             }
         }
+    }
+    export async function highlightTag(highlightedTagInfo:TagInfo) {
+        let linesToCollapse: number[] = [];
+        let highlightedTags = Singleton.getTags().filter(e => e.tagInfo === highlightedTagInfo);
+        (await getMethodsInCurrentFiles())
+        .filter(method => !methodContainsHighlightedTags(method, highlightedTags))
+        .forEach(method => {
+            foldedMethods.push(method.name);
+            linesToCollapse.push(method.location.range.start.line);
+        });
         vscode.commands.executeCommand('editor.fold', {levels: 1, direction: 'up', selectionLines: linesToCollapse});
     }
 }
