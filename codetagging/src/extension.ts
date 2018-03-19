818d00a8a121fcf2a178b3632efd066d530f3bb6
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2018-03-18 18:46:28 -0700

    Refactored extensions

--- codetagging/src/extension.ts
+++ codetagging/src/extension.ts
@@ -27,29 +27,28 @@ function relativeFilePathForDocument(document:vscode.TextDocument):string {
 
 function tagSelection(tagIndex: number) {
     let textEditor = vscode.window.activeTextEditor;
-    if (textEditor !== undefined) {
-        let tags: Tag[] = Singleton.getTags();
-        let tagInfos: TagInfo[] = Singleton.getTagInfos();
-        for (let selection of textEditor.selections) {
-            let tagInfo = tagInfos[tagIndex];
-            let fileName = relativeFilePathForDocument(textEditor.document);
-
-            let tag = new Tag(tagInfo, fileName, selection.start.line + 1, selection.end.line);
-            if (selection.start.line === selection.end.line || selection.end.character > 0) {
-                tag = new Tag(tagInfo, fileName, selection.start.line + 1, selection.end.line + 1);
+    if (textEditor === undefined) {
+        return;
+    }
+    let tags: Tag[] = Singleton.getTags();
+    let tagInfo = Singleton.getTagInfos()[tagIndex];
+    let fileName = relativeFilePathForDocument(textEditor.document);
+    for (let selection of textEditor.selections) {
+        let tag = new Tag(tagInfo, fileName, selection.start.line + 1, selection.end.line);
+        if (selection.start.line === selection.end.line || selection.end.character > 0) {
+            tag = new Tag(tagInfo, fileName, selection.start.line + 1, selection.end.line + 1);
+        }
+        let overlappingTag;
+        for (let oldTag of tags) {
+            if (oldTag.overlaps(tag)) {
+                overlappingTag = oldTag;
             }
-            let overlappingTag;
-            for (let oldTag of tags) {
-                if (oldTag.overlaps(tag)) {
-                    overlappingTag = oldTag;
-                }
-            }
-            if (overlappingTag !== undefined) {
-                overlappingTag.start = Math.min(tag.start, overlappingTag.start);
-                overlappingTag.end = Math.max(tag.end, overlappingTag.end);
-            } else {
-                Singleton.addTag(tag);
-            }
+        }
+        if (overlappingTag !== undefined) {
+            overlappingTag.start = Math.min(tag.start, overlappingTag.start);
+            overlappingTag.end = Math.max(tag.end, overlappingTag.end);
+        } else {
+            Singleton.addTag(tag);
         }
     }
     redraw();
@@ -109,83 +108,13 @@ function filesThatDoNotContainTagInfo(tagInfo:TagInfo):string[]|undefined {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "codetagging" is now active!');
 
-    // uri object that represents the current workspace path
-    let uriString = vscode.workspace.rootPath;
-    console.log(uriString);
+    setupSerializingActions(context);
+    createTagMenu(context);
 
-    //instantiate our class that serializes objects
-    //we pass it the location of the where we want to save the file
-    //this is showing as an error but it works, something about string | undefined cant be assigned to string
-    if (vscode.workspace.rootPath) {
-        let mySerializer = new GenerateSerialization(vscode.Uri.parse(vscode.workspace.rootPath));
-        //command to save taging to disk
-        let saveToDisk = vscode.commands.registerCommand('extension.serialize', () => {
-            //call the serialize method passing it the json of serialized tag objects to write to file
-            mySerializer.serialize();
-        });
-        context.subscriptions.push(saveToDisk);
-
-        let myLoader = new LoadSerialization(vscode.workspace.rootPath);
-
-        let loadFromFile = vscode.commands.registerCommand('extension.load', () => {
-            //call the serialize method passing it the json of serialized tag objects to write to file
-            myLoader.readFile();
-            redraw();
-        });
-        context.subscriptions.push(loadFromFile);
-
-    }
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let tagMenu = vscode.commands.registerCommand('extension.tagMenu', () => {
-        // "Tag selection"
-        // key: ctrl/cmd + shift + T
-        // Command for tagging the currently selected code
-        let existingTags = ["tag1", "tag2", "tag3", "tag4", "testTag1", "testTag2", "testTag3"]; // TODO this should be changed to generate the array from the current list of tag objects
-        existingTags.unshift("Create New Tag"); // add option to create a new tag to array
-
-        // open Quick Pick input box, displays suggestions based on typed text from the existingTags array
-        vscode.window.showQuickPick(existingTags).then(input => {
-            // executes when user presses Enter or selects a suggested tag name
-            // if 'Create New Tag' is selected or entered, open InputBox to enter new tag name
-            // if no tag name is entered or the tag name is not an existing tag, code is not tagged
-            // if an existing tag is entered or selected, add the current code selection to that tag
-            if (typeof input !== 'undefined') {
-                if (input === 'Create New Tag') {
-                    // open InputBox to create a new tag
-                    vscode.window.showInputBox({prompt: 'Enter new tag name'}).then(value => {
-                        // executes when user presses Enter
-                        // if no tag name is entered, no tag is created
-                        // if the entered tag name already exists, no tag is created
-                        // otherwise, create new tag and add the selected code to the new tag
-                        if (!value) {
-                            vscode.window.showErrorMessage('No tag name was entered, code was not tagged');
-                        } else if (existingTags.includes(value)) {
-                            vscode.window.showErrorMessage('Tag already exists, code was not tagged');
-                        } else {
-                            // TODO actually create tag and add code selection to the new tag
-                            vscode.window.showInformationMessage('Tag created, code was tagged with \"' + value + '\"');
-                        }
-                    });
-                } else if (existingTags.includes(input)) {
-                    // TODO actually add current code selection to the chosen tag
-                    vscode.window.showInformationMessage('Code was tagged with \"' + input + '\"');
-                } else {
-                    // shouldn't reach this point
-                    vscode.window.showErrorMessage('Code was not tagged');
-                }
-            } else {
-                vscode.window.showErrorMessage('No tag was selected, code was not tagged');
-            }
-        });
-    });
     let disposable = vscode.commands.registerCommand('extension.tag1', () => {
         tagSelection(0);
     });
@@ -236,9 +165,79 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
     context.subscriptions.push(disposable2);
     context.subscriptions.push(disposable3);
-    context.subscriptions.push(tagMenu);
 }
 
+//instantiate our class that serializes objects
+//we pass it the location of the where we want to save the file
+//this is showing as an error but it works, something about string | undefined cant be assigned to string
+function setupSerializingActions(context:vscode.ExtensionContext) {
+    if (vscode.workspace.rootPath) {
+        let mySerializer = new GenerateSerialization(vscode.Uri.parse(vscode.workspace.rootPath));
+        //command to save taging to disk
+        let saveToDisk = vscode.commands.registerCommand('extension.serialize', () => {
+            //call the serialize method passing it the json of serialized tag objects to write to file
+            mySerializer.serialize();
+        });
+        context.subscriptions.push(saveToDisk);
+
+        let myLoader = new LoadSerialization(vscode.workspace.rootPath);
+
+        let loadFromFile = vscode.commands.registerCommand('extension.load', () => {
+            //call the serialize method passing it the json of serialized tag objects to write to file
+            myLoader.readFile();
+            redraw();
+        });
+        context.subscriptions.push(loadFromFile);
+    }
+}
+
+// The command has been defined in the package.json file
+// Now provide the implementation of the command with  registerCommand
+// The commandId parameter must match the command field in package.json
+function createTagMenu(context:vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.tagMenu', () => {
+        // "Tag selection"
+        // key: ctrl/cmd + shift + T
+        // Command for tagging the currently selected code
+        let existingTags = ["tag1", "tag2", "tag3", "tag4", "testTag1", "testTag2", "testTag3"]; // TODO this should be changed to generate the array from the current list of tag objects
+        existingTags.unshift("Create New Tag"); // add option to create a new tag to array
+
+        // open Quick Pick input box, displays suggestions based on typed text from the existingTags array
+        vscode.window.showQuickPick(existingTags).then(input => {
+            // executes when user presses Enter or selects a suggested tag name
+            // if 'Create New Tag' is selected or entered, open InputBox to enter new tag name
+            // if no tag name is entered or the tag name is not an existing tag, code is not tagged
+            // if an existing tag is entered or selected, add the current code selection to that tag
+            if (typeof input !== 'undefined') {
+                if (input === 'Create New Tag') {
+                    // open InputBox to create a new tag
+                    vscode.window.showInputBox({prompt: 'Enter new tag name'}).then(value => {
+                        // executes when user presses Enter
+                        // if no tag name is entered, no tag is created
+                        // if the entered tag name already exists, no tag is created
+                        // otherwise, create new tag and add the selected code to the new tag
+                        if (!value) {
+                            vscode.window.showErrorMessage('No tag name was entered, code was not tagged');
+                        } else if (existingTags.includes(value)) {
+                            vscode.window.showErrorMessage('Tag already exists, code was not tagged');
+                        } else {
+                            // TODO actually create tag and add code selection to the new tag
+                            vscode.window.showInformationMessage('Tag created, code was tagged with \"' + value + '\"');
+                        }
+                    });
+                } else if (existingTags.includes(input)) {
+                    // TODO actually add current code selection to the chosen tag
+                    vscode.window.showInformationMessage('Code was tagged with \"' + input + '\"');
+                } else {
+                    // shouldn't reach this point
+                    vscode.window.showErrorMessage('Code was not tagged');
+                }
+            } else {
+                vscode.window.showErrorMessage('No tag was selected, code was not tagged');
+            }
+        });
+    }));
+}
 
 // this method is called when your extension is deactivated
 export function deactivate() {
